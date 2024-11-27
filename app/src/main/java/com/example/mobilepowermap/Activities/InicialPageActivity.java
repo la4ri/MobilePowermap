66eafc8f074a795cfdd172f7f738dd853d7c45a7
Larissa <larissamoreiracarvalho@gmail.com>
2024-11-27 16:53:21 -0300

    feat/layout login

--- app/src/main/java/com/example/mobilepowermap/Activities/InicialPageActivity.java
+++ app/src/main/java/com/example/mobilepowermap/Activities/InicialPageActivity.java
@@ -3,6 +3,7 @@ package com.example.mobilepowermap.Activities;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mobilepowermap.ConfigMaps.MapManager;
 import com.example.mobilepowermap.R;
 
 public class InicialPageActivity extends AppCompatActivity {
@@ -11,5 +12,17 @@ public class InicialPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicial_page);
+
+        // Inicializar o mapa
+        MapManager.initializeMap(this, R.id.map);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Passa a resposta da permissão para o MapManager
+        MapManager mapManager = new MapManager(this);
+        mapManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
